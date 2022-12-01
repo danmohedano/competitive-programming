f8c430af3c91695d3fc587acffaf0c2a56c6525e
danmohedano <danimohedano1998@gmail.com>
2022-12-01 17:48:24 +0100

    Advent of Code 2015 - Day 3

--- advent-of-code/2015/main.py
+++ advent-of-code/2015/main.py
@@ -42,6 +42,7 @@ def day2_part1(data_file='data/day2.txt'):
 
         return total
 
+
 def day2_part2(data_file='data/day2.txt'):
     with open(data_file, 'r') as f:
         lines = f.readlines()
@@ -57,8 +58,62 @@ def day2_part2(data_file='data/day2.txt'):
         return total        
 
 
+def day3_part1(data_file='data/day3.txt'):
+    with open(data_file, 'r') as f:
+        line = f.readlines()[0]
+
+        houses = set()
+        pos = (0, 0)
+        houses.add(pos)
+
+        for c in line:
+            new_pos = [pos[0], pos[1]]
+            if c == '>':
+                new_pos[0] += 1
+            elif c == '<':
+                new_pos[0] -= 1
+            elif c == 'v':
+                new_pos[1] += 1
+            else:
+                new_pos[1] -= 1
+
+            pos = tuple(new_pos)
+            houses.add(pos)
+
+        return len(houses)
+
+
+def day3_part2(data_file='data/day3.txt'):
+    with open(data_file, 'r') as f:
+        line = f.readlines()[0]
+
+        houses = set()
+        pos = [(0, 0), (0, 0)]
+        santa = 0
+        houses.add(pos[0])
+
+        for c in line:
+            new_pos = [pos[santa][0], pos[santa][1]]
+            if c == '>':
+                new_pos[0] += 1
+            elif c == '<':
+                new_pos[0] -= 1
+            elif c == 'v':
+                new_pos[1] += 1
+            else:
+                new_pos[1] -= 1
+
+            pos[santa] = tuple(new_pos)
+            houses.add(pos[santa])
+            santa = (santa + 1) % 2
+
+        return len(houses)
+
+
 if __name__ == '__main__':
     print('Result Day 1 Part 1: ', day1_part1())
     print('Result Day 1 Part 2: ', day1_part2())
     print('Result Day 2 Part 1: ', day2_part1())
     print('Result Day 2 Part 2: ', day2_part2())
+    print('Result Day 3 Part 1: ', day3_part1())
+    print('Result Day 3 Part 2: ', day3_part2())
