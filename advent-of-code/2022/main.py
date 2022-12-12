664605354f3b26ff79e973b811f8d457edfc1b78
danmohedano <danimohedano1998@gmail.com>
2022-12-12 16:43:00 +0100

    Advent of Code 2022 - Day 12

--- advent-of-code/2022/main.py
+++ advent-of-code/2022/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import queue
 
 
 TEST_FILE = "data/test.txt"
@@ -768,6 +769,164 @@ def day11_part2(data_file="data/day11.txt"):
         return inspected[-1] * inspected[-2]
 
 
+def day12_part1(data_file="data/day12.txt"):
+    with open(data_file, "r") as f:
+        map = [x.replace('\n', '') for x in f.readlines()]
+        heights = [[0 for _ in range(len(map[0]))] for _ in range(len(map))]
+        start = None
+        end = None
+
+        def valid_neighbors(node, heights):
+            valid = []
+            possible = []
+            for i in range(-1, 2, 1):
+                # Vertical
+                v = (max(min(node[0] + i, len(heights) - 1), 0), node[1])
+                # Horizontal
+                h = (node[0], max(min(node[1] + i, len(heights[0]) - 1), 0))
+                if v != node and v not in possible:
+                    possible.append(v)
+                if h != node and h not in possible:
+                    possible.append(h)
+        
+            for n in possible:
+                if heights[n[0]][n[1]] <= heights[node[0]][node[1]] + 1 and n not in valid:
+                    valid.append(n)
+
+            return valid
+
+        # Read heights and starting/end points 
+        for i in range(len(map)):
+            for j in range(len(map[0])):
+                if map[i][j] == 'S':
+                    heights[i][j] = 0
+                    start = (i, j)
+                elif map[i][j] == 'E':
+                    heights[i][j] = ord('z') - ord('a')
+                    end = (i, j)
+                else:
+                    heights[i][j] = ord(map[i][j]) - ord('a')
+
+        # Perform dijkstra
+        d_prev = {}
+        d_dist = {}
+        s = {}
+        pq = queue.PriorityQueue() 
+
+        # Initialize structures
+        for i in range(len(heights)):
+            for j in range(len(heights[0])):
+                d_prev[(i, j)] = None
+                d_dist[(i, j)] = np.Inf
+                s[(i, j)] = False
+
+        # Push starting node to Queue
+        d_dist[start] = 0
+        pq.put((d_dist[start], start))
+
+        # Dijkstra loop
+        while not pq.empty():
+            _, v = pq.get()
+            if v == end:
+                break
+            if not s[v]:
+                s[v] = True
+                # If the node has not been checked 
+                valid = valid_neighbors(v, heights)
+
+                for next in valid:
+                    # If better path found, update it
+                    if d_dist[next] > d_dist[v] + 1:
+                        d_dist[next] = d_dist[v] + 1
+                        d_prev[next] = v
+                        pq.put((d_dist[next], next))
+
+        return d_dist[end]
+
+
+def day12_part2(data_file="data/day12.txt"):
+    with open(data_file, "r") as f:
+        map = [x.replace('\n', '') for x in f.readlines()]
+        heights = [[0 for _ in range(len(map[0]))] for _ in range(len(map))]
+        possible_starts = []
+        end = None
+
+        def valid_neighbors(node, heights):
+            valid = []
+            possible = []
+            for i in range(-1, 2, 1):
+                # Vertical
+                v = (max(min(node[0] + i, len(heights) - 1), 0), node[1])
+                # Horizontal
+                h = (node[0], max(min(node[1] + i, len(heights[0]) - 1), 0))
+                if v != node and v not in possible:
+                    possible.append(v)
+                if h != node and h not in possible:
+                    possible.append(h)
+        
+            for n in possible:
+                if heights[n[0]][n[1]] <= heights[node[0]][node[1]] + 1 and n not in valid:
+                    valid.append(n)
+
+            return valid
+
+        # Read heights and starting/end points 
+        for i in range(len(map)):
+            for j in range(len(map[0])):
+                if map[i][j] == 'S':
+                    heights[i][j] = 0
+                    possible_starts.append((i, j))
+                elif map[i][j] == 'E':
+                    heights[i][j] = ord('z') - ord('a')
+                    end = (i, j)
+                else:
+                    heights[i][j] = ord(map[i][j]) - ord('a')
+                    if heights[i][j] == 0:
+                        possible_starts.append((i, j))
+
+        # Iterate dijkstra for all possible starts
+        shortest_path = np.Inf
+
+        for start in possible_starts:
+            # Perform dijkstra
+            d_prev = {}
+            d_dist = {}
+            s = {}
+            pq = queue.PriorityQueue() 
+
+            # Initialize structures
+            for i in range(len(heights)):
+                for j in range(len(heights[0])):
+                    d_prev[(i, j)] = None
+                    d_dist[(i, j)] = np.Inf
+                    s[(i, j)] = False
+
+            # Push starting node to Queue
+            d_dist[start] = 0
+            pq.put((d_dist[start], start))
+
+            # Dijkstra loop
+            while not pq.empty():
+                _, v = pq.get()
+                if v == end:
+                    break
+                if not s[v]:
+                    s[v] = True
+                    # If the node has not been checked 
+                    valid = valid_neighbors(v, heights)
+
+                    for next in valid:
+                        # If better path found, update it
+                        if d_dist[next] > d_dist[v] + 1:
+                            d_dist[next] = d_dist[v] + 1
+                            d_prev[next] = v
+                            pq.put((d_dist[next], next))
+
+            shortest_path = min(shortest_path, d_dist[end])
+
+        return shortest_path
+
+
 if __name__ == "__main__":
     print("Result Day 1 Part 1: ", day1_part1())
     print("Result Day 1 Part 2: ", day1_part2())
@@ -791,3 +950,5 @@ if __name__ == "__main__":
     print("Result Day 10 Part 2: ", day10_part2())
     print("Result Day 11 Part 1: ", day11_part1())
     print("Result Day 11 Part 2: ", day11_part2())
+    print("Result Day 12 Part 1: ", day12_part1())
+    print("Result Day 12 Part 2: ", day12_part2())
